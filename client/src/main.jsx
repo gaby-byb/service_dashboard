1080c22babd1402d184c56ccd66c034cbc4c326a
Gaby Barnes <gabyatl@outlook.com>
2026-03-10 17:02:51 -0400

    Reset client: rebuilt frontend with clean Vite setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,10 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { TooltipProvider } from "./components/ui/tooltip.jsx";
-import App from "./App.jsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <TooltipProvider>
-      <App />
-    </TooltipProvider>
-  </BrowserRouter>,
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
