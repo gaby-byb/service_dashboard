cebc64830964dd0748832b525909779e445d2f16
Gaby Barnes <gabyatl@outlook.com>
2026-03-10 11:40:58 -0400

    add shadcn/ui configuration and alias setup

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <>
+      <div className="p-10 text-4xl font-bold text-red-500">
+        Everything will live here
+      </div>
+    </>
+  );
+}
