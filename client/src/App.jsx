2c664972c915fc178aa9155022662ca28055d25e
Gaby Barnes <gabyatl@outlook.com>
2026-03-10 20:50:17 -0400

    Login page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 import "./App.css";
-
 function App() {
-  return <div className="bg-red-500 text-white p-4">Tailwind test</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+    // <>
+    //   <div className="bg-red-500 text-white p-4">Tailwind test</div>
+    //   <Button variant="outline"> Click me </Button>
+    // </>
+  );
 }
 
 export default App;
