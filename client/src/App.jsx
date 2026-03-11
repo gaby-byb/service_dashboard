23da04c537bd47ab8d50bb31d2bf7f4684c85e11
Gaby Barnes <gabyatl@outlook.com>
2026-03-11 12:41:29 -0400

    Sidebar and dark mode added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import { ThemeProvider } from "@/components/theme-provider"; // Ensure this path is correct
 import "./App.css";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+
     // <>
     //   <div className="bg-red-500 text-white p-4">Tailwind test</div>
     //   <Button variant="outline"> Click me </Button>
