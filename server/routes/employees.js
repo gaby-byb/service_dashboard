4036cb8a032839715f35a2158687ad6ad5831167
Gaby Barnes <gabyatl@outlook.com>
2026-05-05 14:54:31 -0400

    employee routes added

--- server/routes/employees.js
+++ server/routes/employees.js
@@ -34,5 +34,49 @@ router.get("/:id", async (req, res) => {
     res.status(500).send("GET Database error");
   }
 });
+//POST
 
+router.post("/", async (req, res) => {
+  try {
+    const { name, phone, email } = req.body;
+    const result = await pool.query(
+      "INSERT INTO employees (name, phone, email) VALUES $1, $2, $3"[
+        (name, phone, email)
+      ],
+    );
+  } catch (error) {
+    res.status(500).send("POST database error");
+  }
+});
+
+//PUT /employees/:id
+router.put("/:id", async (req, res) => {
+  try {
+    const { name, phone, email } = req.body;
+    const { id } = req.params;
+
+    const result = await pool.query(
+      "UPDATE employees set name=$1, phone=$2, email=$3"[(name, phone, email)],
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).send("PUT database error");
+  }
+});
+
+// DELETE /employees/:id
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await pool.query(
+      "DELETE from employees WHERE  id=$1 RETURNING *",
+      [id],
+    );
+    //send this data back to client as json
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).send("Delete failed");
+  }
+});
 export default router;
